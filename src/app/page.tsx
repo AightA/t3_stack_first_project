5c1eaa88e1f1b990f0313ed979f387093c7429f8
Ahmed Merehil <ahmed8merehil@yahoo.com>
2024-07-02 14:41:00 +0100

    refactored sign in with getServerAuthSession
    Signed-off-by: Ahmed Merehil <ahmed8merehil@yahoo.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
-export default function HomePage() {
-  return (
+import { getServerAuthSession } from "~/server/auth";
+
+export default async function HomePage() {
+  const result = await getServerAuthSession();
+
+  return result ? (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
         <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
@@ -8,5 +12,10 @@ export default function HomePage() {
         <div>New page of NextJS</div>
       </div>
     </main>
+  ) : (
+    <>
+      <div>You need sign in to create a new</div>
+      <button type="button">Sign up</button>
+    </>
   );
 }
