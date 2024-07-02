c869370b67832b03110d233e5886ad928c02f3e6
Ahmed Merehil <ahmed8merehil@yahoo.com>
2024-07-02 14:54:15 +0100

    added sign in condtion
    Signed-off-by: Ahmed Merehil <ahmed8merehil@yahoo.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import { getServerAuthSession } from "~/server/auth";
 
 export default async function HomePage() {
-  const result = await getServerAuthSession();
+  
+  const getSignIn = await getServerAuthSession();
 
-  return result ? (
+  return getSignIn ? (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
         <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
