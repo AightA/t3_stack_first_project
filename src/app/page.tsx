13fed549b4e1c076c57d18acfdc363464c7bebc9
Ahmed Merehil <ahmed8merehil@yahoo.com>
2024-07-02 14:56:57 +0100

    added sign in btn
    Signed-off-by: Ahmed Merehil <ahmed8merehil@yahoo.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { getServerAuthSession } from "~/server/auth";
 
 export default async function HomePage() {
-  
   const getSignIn = await getServerAuthSession();
 
   return getSignIn ? (
@@ -11,12 +10,19 @@ export default async function HomePage() {
           Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
         </h1>
         <div>New page of NextJS</div>
+        <button className="rounded border-b-4 border-blue-700 bg-blue-500 px-4 py-2 font-bold text-white hover:border-blue-500 hover:bg-blue-400">
+          Sign In
+        </button>
       </div>
     </main>
   ) : (
     <>
-      <div>You need sign in to create a new</div>
-      <button type="button">Sign up</button>
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+        <div>You need sign in to create a new</div>
+        <button className="rounded border-b-4 border-blue-700 bg-blue-500 px-4 py-2 font-bold text-white hover:border-blue-500 hover:bg-blue-400">
+          Sign In
+        </button>
+      </main>
     </>
   );
 }
